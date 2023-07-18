63dbd6b4c8a648743bc5c1f349584b1d446623d8
Nikita Dovhich <xennerit@icloud.com>
2023-07-18 04:25:02 +0300

    Delete src/pages/recipes directory

--- src/pages/recipes/StyledRecipes.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { rootPageContainer } from '~/utils/_mixins';
-
-export const StyledRecipesContainer = styled.section`
-  ${rootPageContainer}
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-`;
